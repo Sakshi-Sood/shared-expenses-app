29fdc7dd71531c34fa09f6e8d6a60b4797829373
Sakshi-Sood <sakshisood004@gmail.com>
2026-06-15 08:36:39 +0530

    Add group and membership models

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -1,3 +1,47 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="created_groups"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Membership(models.Model):
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name="memberships"
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="memberships"
+    )
+
+    joined_at = models.DateField()
+
+    left_at = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ("group", "user")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.group.name}"
